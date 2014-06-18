ed173ea37a276f630c8dff2e9d0308b3c973cace
James Kuczynski <james_kuczynski@student.uml.edu>
2014-06-18 14:40:43 -0400

    Changed path, and replaced 'goehring' with my own login name

--- data/runDemo.sh
+++ data/runDemo.sh
@@ -1,6 +1,6 @@
 rostopic pub /bolt/vision/record std_msgs/String "test"
 ../who-imagenet-adapt/who_imagenet_adapt.sh $*
-cp model.ffld.*.txt /home/goehring/ros_workspace/ffld/outoffice/
+cp model.ffld.*.txt /home/james/ros_workspace/raptor/ffld_ros/ffld/outoffice/
 CATEGORYNAME=`echo $* | sed 's/ /_/g'`
 sed "s/newObject/$CATEGORYNAME/g" demo-office.config.template > $HOME/ros_workspace/bolt/ros/vision/kinect/demo-office.config 
 
